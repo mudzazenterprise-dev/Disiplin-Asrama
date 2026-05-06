8362285efaf5aece419d362802cd48b9c467b749
MUDZAZ <mudzazenterprise@gmail.com>
2026-05-06 23:14:37 +0800

    fix local postgres

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'smknyalas'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL") or "sqlite:///disiplin.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
